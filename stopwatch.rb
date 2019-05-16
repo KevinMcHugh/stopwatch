a927181c75ac64d874b10742bd56af50ef98faef
Kevin McHugh <kev@kevinmchugh.me>
2019-05-16 17:03:03 -0500

    Formats time nicely, and adds end alert

--- stopwatch.rb
+++ stopwatch.rb
@@ -1,5 +1,7 @@
 require 'curses'
 Curses.init_screen
+time_interval = 1
+
 begin
 	x = Curses.lines / 2  # We will center our text
 	y = Curses.cols / 2
@@ -11,12 +13,25 @@ begin
 	message.addstr("This is a timer.")
 	message.refresh
 
-	max = 5
-	(1..max).each do |i|
+	mappings = { "push ups" => 1, "shadow boxing" => 3, "sit ups" => 1 }
+	max = 0.1
+	max_seconds = max * 60
+	(0..max_seconds).each do |i|
+		seconds_remaining = (max_seconds -i).to_i
+		minutes = seconds_remaining / 60
+		seconds = seconds_remaining % 60
 		timer.clear
-		timer.addstr("#{max - i} / #{max}")
+		timer.addstr("#{minutes}:#{seconds} / #{max_seconds}")
 		timer.refresh
-		sleep 1
+		sleep time_interval
+	end
+	3.times do
+		message.clear
+		message.refresh
+		sleep time_interval / 10.0
+		message.addstr("Time's up!!!")
+		message.refresh
+		sleep time_interval / 2.0
 	end
 ensure
 	Curses.close_screen
