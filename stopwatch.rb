5884219ca108fd752c5cf9626252e672528353e4
Kevin McHugh <kev@kevinmchugh.me>
2019-05-16 16:46:53 -0500

    Builds a two panel timer

--- stopwatch.rb
+++ stopwatch.rb
@@ -1,12 +1,22 @@
 require 'curses'
 Curses.init_screen
 begin
-	x = Curses.cols / 2  # We will center our text
-	y = Curses.lines / 2
-	Curses.setpos(y, x)  # Move the cursor to the center of the screen
-	Curses.addstr("Press any key to exit.")
-	Curses.refresh
-	Curses.getch
+	x = Curses.lines / 2  # We will center our text
+	y = Curses.cols / 2
+	# Curses.setpos(1, 1)  # Move the cursor to the center of the screen
+	message = Curses::Window.new(x,y,x,y-1)
+
+	timer = Curses::Window.new(x, y, x + 1, y + 1)
+
+	message.addstr("This is a timer.")
+	message.refresh
+
+	(1..5).each do |i|
+		timer.clear
+		timer.addstr(i.to_s)
+		timer.refresh
+		sleep 1
+	end
 ensure
 	Curses.close_screen
 end
