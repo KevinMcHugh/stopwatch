5abaddf915955bfd90500bd1ee180720b4514028
Kevin McHugh <kev@kevinmchugh.me>
2019-05-16 16:31:31 -0500

    I learned how to use curses. FUCK!

---
+++ stopwatch.rb
@@ -0,0 +1,12 @@
+require 'curses'
+Curses.init_screen
+begin
+	x = Curses.cols / 2  # We will center our text
+	y = Curses.lines / 2
+	Curses.setpos(y, x)  # Move the cursor to the center of the screen
+	Curses.addstr("Press any key to exit.")
+	Curses.refresh
+	Curses.getch
+ensure
+	Curses.close_screen
+end
